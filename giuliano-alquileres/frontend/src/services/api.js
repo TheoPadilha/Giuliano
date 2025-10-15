9abe853fd3f3ce3920f6e0a8f3c482dc4f4d9558
TheoPadilha <168146283+TheoPadilha@users.noreply.github.com>
2025-10-15 15:04:41 -0300

    commit

--- giuliano-alquileres/frontend/src/services/api.js
+++ giuliano-alquileres/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001/api";
+const API_URL = import.meta.env.VITE_API_URL || "https://giulianoa-backend.onrender.com/";
 
 // Instância principal do axios
 const api = axios.create({
